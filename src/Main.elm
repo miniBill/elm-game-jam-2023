4fa9ac7f81bcabb3ecbe7f4866f4d1e3cefc892f
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-09-11 14:34:42 +0200

    Hm

--- src/Main.elm
+++ src/Main.elm
@@ -1,31 +1,95 @@
 module Main exposing (Model, main)
 
-import Playground exposing (Computer, Msg, Playground, Shape, game, move, red, square, toX, toY)
+import MyPlayground exposing (polygon, render)
+import Playground exposing (Computer, Keyboard, Msg, Playground, Shape, game, red)
+import Set
 
 
 type alias Model =
-    ( Float, Float )
+    { points :
+        List
+            { latitude : Float
+            , longitude : Float
+            }
+    }
 
 
 main : Program () (Playground Model) Msg
 main =
-    game view update ( 0, 0 )
+    game view update init
 
 
-view : a -> Model -> List Shape
-view computer ( x, y ) =
-    [ square red 40
-        |> move x y
+init : Model
+init =
+    { points = [] }
+
+
+view : Computer -> Model -> List Shape
+view computer { points } =
+    let
+        mx : Float
+        mx =
+            computer.mouse.x
+
+        my : Float
+        my =
+            computer.mouse.y
+    in
+    [ polygon red
+        [ ( mx, my )
+        , ( mx + 2, my - 20 )
+        , ( mx + 10, my - 15 )
+        ]
     ]
+        |> render computer
 
 
 update : Computer -> Model -> Model
-update computer ( x, y ) =
-    ( x + toX computer.keyboard
-    , y + toY computer.keyboard
-    )
+update computer model =
+    -- let
+    --     dt : Float
+    --     dt =
+    --         toFloat computer.time.delta
+    --     newX : Float
+    --     newX =
+    --         computer.mouse.x
+    --     newY : Float
+    --     newY =
+    --         computer.mouse.y
+    -- in
+    model
 
 
-reproject : Float -> Float -> Float -> Float -> Float -> Float
-reproject fromMin fromMax toMin toMax value =
+toX : Keyboard -> Float
+toX keyboard =
+    (boolToNumber <| keyboard.right || Set.member "KeyD" keyboard.keys)
+        - (boolToNumber <| keyboard.left || Set.member "KeyA" keyboard.keys)
+
+
+toY : Keyboard -> Float
+toY keyboard =
+    (boolToNumber <| keyboard.up || Set.member "KeyW" keyboard.keys)
+        - (boolToNumber <| keyboard.down || Set.member "KeyS" keyboard.keys)
+
+
+boolToNumber : Bool -> number
+boolToNumber b =
+    if b then
+        1
+
+    else
+        0
+
+
+screenToWorld : Computer -> ( Float, Float ) -> ( Float, Float )
+screenToWorld computer ( x, y ) =
+    if computer.screen.width > computer.screen.height then
+        ( x, y )
+
+    else
+        ( x, y )
+
+
+project : Float -> Float -> Float -> Float -> Float -> Float
+project fromMin fromMax toMin toMax value =
     (value - fromMin) / (fromMax - fromMin) * (toMax - toMin) + toMin
